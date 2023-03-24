34646ab377748909cb4b66980daaa11308e9c07f
NazarFedus <simpleprograming7@gmail.com>
2023-03-24 21:26:35 +0000

    start server

---
+++ authController.js
@@ -0,0 +1,32 @@
+const {validationResult} = require('express-validator');
+
+class authController {
+     async registration(req, res){
+          try{
+               const errors = validationResult(req);
+               if(!errors.isEmpty()){
+                    return res.status
+               }
+
+          } catch(e){
+               console.log(e);
+          }
+     }
+
+     async login(req, res){
+          try{
+
+          } catch(e){
+               console.log(e);
+          }
+     }
+
+     async getUsers(req, res){
+          try{
+               res.json("server work")
+          } catch(e){
+               console.log(e);
+          }
+     }
+}
+module.exports = new authController();
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRouter = require('./authRouter')
+
+const PORT = process.env.PORT || 5000
+
+const app = express();
+
+app.use(express.json())
+app.use('/auth', authRouter)
+
+const start = async () => {
+     try{
+          await mongoose.connect('mongodb+srv://nazarfedus5:8O50DhggdOxItWhK@cluster0.qwouo7o.mongodb.net/?retryWrites=true&w=majority');
+
+          app.listen(PORT, () => {
+               console.log(`Server started on port: ${PORT}...`);
+          })
+     } catch(e){
+          console.log(e);
+     }
+}
+
+start();
