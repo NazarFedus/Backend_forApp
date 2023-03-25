a59cf913577d057128f2e79dc6f3eecb78cbb19f
NazarFedus <simpleprograming7@gmail.com>
2023-03-25 10:12:20 +0000

    add login logic

--- authController.js
+++ authController.js
@@ -1,6 +1,17 @@
 const User = require('./models/User');
 const {validationResult} = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {secret} = require('./config');
+
+
+const generateAccessToken = (id, email) => {
+     const payload = {
+          id,
+          email
+     }
+     return jwt.sign(payload, secret, {expiresIn: "24h"});
+}
 
 class authController {
      async registration(req, res){
@@ -24,9 +35,23 @@ class authController {
 
      async login(req, res){
           try{
+               const {email, password} = req.body;
+               const user = await User.findOne({email});
+               if(!user){
+                    return res.status(400).json({message: "User not found"});
+               }
+
+               const validPassword = bcrypt.compareSync(password, user.password);
+               if(!validPassword){
+                    return res.status(400).json({message: "Invalid password!"});
+               }
+
+               const token = generateAccessToken(user._id, user.email);
+               return res.json({token});
 
           } catch(e){
                console.log(e);
+               res.status(400).json({message: "LogIn error."})
           }
      }
 
