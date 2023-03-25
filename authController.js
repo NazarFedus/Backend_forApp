6bf2274231d7b00226d2c21e2d267733129dfa9b
NazarFedus <simpleprograming7@gmail.com>
2023-03-25 09:52:42 +0000

    add registration logic

--- authController.js
+++ authController.js
@@ -1,15 +1,24 @@
+const User = require('./models/User');
 const {validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 class authController {
      async registration(req, res){
           try{
                const errors = validationResult(req);
                if(!errors.isEmpty()){
-                    return res.status
+                    return res.status(400).json({message: "Registration error", errors})
                }
+
+               const {username, email, password} = req.body;
+               const hashPassword = bcrypt.hashSync(password, 7);
+               const newUser = new User({username, email, password: hashPassword})
+               await newUser.save();
+               return res.json({message: "User was created!"});
 
           } catch(e){
                console.log(e);
+               res.status(400).json({message: "Registration error."});
           }
      }
 
