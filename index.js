2db80a21dc5ad07311b7a98a534102118be0e811
NazarFedus <simpleprograming7@gmail.com>
2023-03-25 11:05:35 +0000

    connect with frontend

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const authRouter = require('./authRouter')
 
 const PORT = process.env.PORT || 5000
 
 const app = express();
+app.use(cors({
+     origin: '*'
+}));
+
+app.use(cors({
+     methods: 'GET, POST'
+}));
+app.use(cors({
+     allowedHeaders: 'Content-Type, Authorization'
+}));
+
 
 app.use(express.json())
 app.use('/auth', authRouter)
